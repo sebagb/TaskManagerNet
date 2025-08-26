3f1fa4497ef1fd62d8a4f4b144b5e2ac9c427246
sebagb <sgbcmpu@gmail.com>
2025-08-26 15:26:07 +0200

    Create ApiEndpoints class

---
+++ TaskManager.Api/ApiEndpoints.cs
@@ -0,0 +1,10 @@
+namespace TaskManager.Api;
+
+public static class ApiEndpoints
+{
+    public static class Project
+    {
+        private const string Base = "projects";
+        public const string CreateProject = $"{Base}";
+    }
+}
