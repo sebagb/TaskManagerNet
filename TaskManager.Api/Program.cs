8eb8135b517d4a12069090d5688be617e12d5735
sebagb <sgbcmpu@gmail.com>
2025-10-05 15:59:20 +0200

    Configure Swagger to accept JWT authorization

--- TaskManager.Api/Program.cs
+++ TaskManager.Api/Program.cs
@@ -1,16 +1,12 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using TaskManager.Api.Auth;
 using TaskManager.Application;
 using TaskManager.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var connectionString = builder.Configuration["Database:ConnectionString"];
 builder.Services.AddApplication();
@@ -40,6 +36,44 @@ builder.Services.AddAuthentication()
 builder.Services.AddAuthorizationBuilder()
     .AddPolicy(AuthConstants.AdminUserClaimName,
         p => p.RequireClaim(AuthConstants.AdminUserClaimName, "true"));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "TaskManager.Api",
+        Version = "v1"
+    });
+
+    c.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme,
+        new OpenApiSecurityScheme
+        {
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Description = "Please enter 'Bearer' followed by your JWT token",
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = JwtBearerDefaults.AuthenticationScheme,
+            BearerFormat = "JWT"
+        });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
